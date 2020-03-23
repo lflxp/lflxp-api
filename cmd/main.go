0d69797bee92627fecfc7fbebcdc9118fde0b7ee
lflxp <lixueping@cloudwalk.cn>
2020-03-23 17:44:05 +0800

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -2,33 +2,32 @@ package main
 
 import (
 	"flag"
+
 	"github.com/lflxp/lflxp-api/pkg"
 )
 
 var (
-	host string
-	port string
+	host  string
+	port  string
 	stats bool
 )
 
 func init() {
-	flag.StringVar(&host,"host","127.0.0.1","bind http host")
-	flag.StringVar(&port,"port","8080","bind http port")
-	flag.BoolVar(&stats,"stats",false,"is output db stats")
+	flag.StringVar(&host, "host", "127.0.0.1", "bind http host")
+	flag.StringVar(&port, "port", "8080", "bind http port")
+	flag.BoolVar(&stats, "stats", false, "is output db stats")
 	flag.Parse()
 }
 
 func main() {
 	api := pkg.Apis{
-		Host:host,
-		Port:port,
+		Host:  host,
+		Port:  port,
 		Stats: stats,
 	}
 
-	err := api.Check()
+	err := api.Execute()
 	if err != nil {
 		panic(err)
 	}
-
-	api.Execute()
 }
